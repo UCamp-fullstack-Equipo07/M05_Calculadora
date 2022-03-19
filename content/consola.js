4f8b4093862ee9ae7e35759305672ede9fbc0cfe
Febolutions <52832816+Febolutions@users.noreply.github.com>
2022-03-19 01:07:39 -0600

    console calculator complete

--- content/consola.js
+++ content/consola.js
@@ -6,27 +6,57 @@ function printOperations() {
 
     switch (option) {
         case '1':
-            console.log(option);
+            console.log('Elegiste la opción ' + option + ': suma');
             let val = prompt('Ingresa el primer numero');
             let val1 = Number(val);
-            console.log(val1)
+            console.log('El primer valor es: ' + val1)
             let val2 = prompt('Ingresa el segundo valor');
             let valu2 = Number(val2);
-            console.log(valu2)
+            console.log('El segundo valor es: ' + valu2);
             let suma = val1 + valu2;
-            console.log(suma)
-            alert(suma);
+            console.log('El resultado es: ' + suma);
+            alert('El resultado de la operación es: ' + suma);
             break;
         case '2':
-            console.log(option);
-            let value1 = prompt('Ingresa el primer numero');
-            x1 = Number(value1);
+            console.log('Elegiste la opción ' + option + ': resta');
+            let x1 = prompt('Ingresa el primer numero');
+            let x = Number(x1);
+            console.log('El primer valor es: ' + x)
+            let y1 = prompt('Ingresa el segundo valor');
+            let y = Number(y1);
+            console.log('El segundo valor es: ' + y)
+            let resta = x - y;
+            console.log('El resultado es: ' + resta)
+            alert('El resultado de la operación es: ' + resta);
             break;
         case '3':
-            console.log(option)
+            console.log('Elegiste la opción ' + option + ': multiplicación');
+            let value1 = prompt('Ingresa el primer numero');
+            let valueFirst = Number(value1);
+            console.log('El primer valor es: ' + valueFirst)
+            let value2 = prompt('Ingresa el segundo valor');
+            let valueSecond = Number(value2);
+            console.log('El segundo valor es: ' + valueSecond)
+            let multiplicación = valueFirst * valueSecond;
+            console.log('El resultado es: ' + multiplicación)
+            alert('El resultado de la operación es: ' + multiplicación);
             break;
         case '4':
-            console.log(option)
+            console.log('Elegiste la opción ' + option + ': división');
+            let va1 = prompt('Ingresa el primer numero');
+            let firstValue = Number(va1);
+            console.log('El primer valor es: ' + firstValue)
+            let va2 = prompt('Ingresa el segundo valor');
+            let secondValue = Number(va2);
+            if (secondValue == 0) {
+                console.log('El segundo valor es ' + secondValue + ' y no se puede dividir')
+                alert('No se puede dividir entre ' + secondValue);
+            } else {
+                console.log('El segundo valor es: ' + secondValue)
+                let division = firstValue / secondValue;
+                console.log('El resultado es: ' + division)
+                alert('El resultado de la operación es: ' + division);
+            }
             break;
     }
 }
